52f9ab4a87ec40e3a6df8570237d6a9bb9522fab
lin MA <lin.ma@ieseg.fr>
2023-11-11 00:38:44 +0100

    Listening to an event

--- javascript/application.js
+++ javascript/application.js
@@ -1,2 +1,14 @@
 import { Application, Controller } from 'stimulus';
 window.Stimulus = Application.start()
+
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", (event) => {
+  console.log("I was clicked");
+  event.currentTarget.classList.remove("btn-primary");
+  event.currentTarget.classList.add("btn-danger");
+  event.currentTarget.innerText = "Bingo!";
+ })
+
+
+
